17ae0def273f11cd05305df0f18513eda1457d9e
wremsen <wcremsen@gmail.com>
2023-12-05 22:34:41 -0500

    almost done with bubble sort, christina just needs to add one small, very small thing

--- practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/callbacks.js
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/callbacks.js
@@ -88,28 +88,87 @@
 // addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
 
 
-Function.prototype.myBind = function (context) {
-    return () => {
-        this.apply(context)
+// Function.prototype.myBind = function (context) {
+//     return () => {
+//         this.apply(context)
+//     }
+// }
+
+// class Lamp {
+//     constructor() {
+//       this.name = "a lamp";
+//     }
+//   }
+  
+//   const turnOn = function() {
+//     console.log("Turning on " + this.name);
+//   };
+  
+//   const lamp = new Lamp();
+  
+//   turnOn(); // should not work the way we want it to
+  
+//   const boundTurnOn = turnOn.bind(lamp);
+//   const myBoundTurnOn = turnOn.myBind(lamp);
+  
+//   boundTurnOn(); // should say "Turning on a lamp"
+//   myBoundTurnOn(); // should say "Turning on a lamp"
+
+
+
+
+
+
+
+
+
+
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Write this first.
+function askIfGreaterThan(el1, el2, callback) {
+  // Prompt user to tell you whether el1 > el2; pass true back to the
+  // callback if true; else false.
+  reader.question(`Sup, is ${el1} > ${el2}? Enter Yes or No: `, function(answer) {
+    if (answer === "Yes") {
+        callback(true)
+    } else {
+        callback(false)
     }
+  });
+};
+
+// Once you're done testing askIfGreaterThan with dummy arguments, write this.
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+  // Do an "async loop":
+  // 1. If (i == arr.length - 1), call outerBubbleSortLoop, letting it
+  //    know whether any swap was made.
+  // 2. Else, use `askIfGreaterThan` to compare `arr[i]` and `arr[i +
+  //    1]`. Swap if necessary. Call `innerBubbleSortLoop` again to
+  //    continue the inner loop. You'll want to increment i for the
+  //    next call, and possibly switch madeAnySwaps if you did swap.
+  
 }
 
-class Lamp {
-    constructor() {
-      this.name = "a lamp";
-    }
+// Once you're done testing innerBubbleSortLoop, write outerBubbleSortLoop.
+// Once you're done testing outerBubbleSortLoop, write absurdBubbleSort.
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+  function outerBubbleSortLoop(madeAnySwaps) {
+    // Begin an inner loop if you made any swaps. Otherwise, call
+    // `sortCompletionCallback`.
   }
-  
-  const turnOn = function() {
-    console.log("Turning on " + this.name);
-  };
-  
-  const lamp = new Lamp();
-  
-  turnOn(); // should not work the way we want it to
-  
-  const boundTurnOn = turnOn.bind(lamp);
-  const myBoundTurnOn = turnOn.myBind(lamp);
-  
-  boundTurnOn(); // should say "Turning on a lamp"
-  myBoundTurnOn(); // should say "Turning on a lamp"
+
+  // Kick the first outer loop off, starting `madeAnySwaps` as true.
+}
+
+absurdBubbleSort([3, 2, 1], function(arr) {
+  console.log("Sorted array: " + JSON.stringify(arr));
+  reader.close();
+});
+
