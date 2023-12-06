62b82c171f5603c61b45cf20bfc3c1f90f2144c0
Yaqi Fang <cfang322@gmail.com>
2023-12-06 14:33:29 -0500

    Finish Clock

--- practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/callbacks.js
+++ practice-for-ch-js-intro-to-callbacks-exercises-long-practice-main/callbacks.js
@@ -1,15 +1,50 @@
 class Clock {
     constructor() {
-        this.date = (Date.now)
-        this.hours = (this.date).GetHours
-
+        this.date = new Date("December 06, 2023 14:01:00");
+        this.hours = this.date.getHours();
+        this.minutes = this.date.getMinutes();
+        this.seconds = this.date.getSeconds();
+        setInterval(this._tick.bind(this), 1000);
     }
 
+    printTime() {
+        console.log(`${this.hours}:${this.minutes}:${this.seconds}`)
+    }
 
+    _tick() {
+        this._incrementSeconds();
+        this.printTime();
+    }
 
+    _incrementSeconds() {
+        this.seconds++
+        if (this.seconds === 60){
+            this.seconds = 0;
+            this._incrementMinutes();
+        }
+    }
 
+    _incrementMinutes() {
+        this.minutes++
+        if (this.minutes === 60){
+            this.minutes = 0;
+            this._incrementHours();
+        }
+    }
 
-
-
-
+    _incrementHours() {
+        this.hours++
+        if (this.hours === 24){
+            this.hours = 0;
+        }
+    }
 }
+
+const clock = new Clock();
+
+
+
+
+
+
+
